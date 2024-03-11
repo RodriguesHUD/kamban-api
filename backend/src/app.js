ebe9710ce92504c50d92c1376320ebd10ba9f734
unknown <hudsondelimarodrigues@hotmail.com>
2024-03-11 17:21:37 -0300

    added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const router = require("./router");
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use(cors());
 
 module.exports = app;
